9c68af7117596c44ec661c71da1398448300cb75
Stacey Watro <Staceywatr@gmail.com>
2015-11-02 13:45:31 -0500

    Changes port from 3000 to 5000.

--- app.js
+++ app.js
@@ -29,7 +29,7 @@ app.get('/data/:id', function (req, res) {
 	})
 });
 
-var server = app.listen(3000, function () {
+var server = app.listen(5000, function () {
   var host = server.address().address;
   var port = server.address().port;
 
